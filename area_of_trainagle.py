66ac4d839d03333dfdb51b6ae362552bfcde7be2
Ashwani <aswanibtech@gmail.com>
2022-07-20 11:45:52 +0530

    area of triangle

---
+++ area_of_trainagle.py
@@ -0,0 +1,9 @@
+#import math
+#import math as m
+from math import *
+a,b,c=4,5,6
+s=(a+b+c)/2
+#area=math.sqrt(s*(s-a)*(s-b)*(s-c))
+#area=m.sqrt(s*(s-a)*(s-b)*(s-c))
+area=sqrt(s*(s-a)*(s-b)*(s-c))
+print("area=",area)
